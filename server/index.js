1ba2da07e5c3e358a1c90bb20278121bb876754a
May Nir <maynirrr@gmail.com>
2022-07-26 23:38:34 +0300

    save user to json file set cookie

--- server/index.js
+++ server/index.js
@@ -1,31 +1,34 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3001;
 const fs = require('fs');
 const bp = require('body-parser')
+const sessions = {};
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', express.static(__dirname + '/'));
+app.use(cookieParser());
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
 app.post("/sign-in", (req, res) => {
-  console.log(req.body);
   const userData = {};
-  userData[req.body.email] = req.body.password;
-  // const userData = { email: req.body.email, password: req.body.password };
+  const email = req.body.email;
+  const password = req.body.password;
+  userData[email] = password;
   fs.writeFile('./users.json', JSON.stringify(userData), 'utf8', function (err) {
     if (err) {
       console.log("An error occured while writing JSON Object to File.");
       return console.log(err);
     }
-
-    console.log("JSON file has been saved.");
+    console.log(`Created new user with email: ${ email }`);
   });
-  res.json({ message: "Hello from server!" });
+  setCookieAndSession(email, password, res);
+  res.end();
 });
 
 app.get('/', (req, res) => {
@@ -35,3 +38,11 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port: ${ port }`);
 })
+
+function setCookieAndSession(email, password, res) {
+  res.cookie('email', email);
+  sessions[email] = password;
+  setTimeout(() => {
+    delete sessions[email];
+  }, 1000 * 60 * 30);
+}
